5c44032b33c1efb31a678692497e2f63ba43a4e7
Lukas Höppner <lukas.hoeppner@gmail.com>
2020-11-06 23:12:40 +0100

    Very rudimentary character controls

---
+++ OmaWatchUnity/Assets/Scripts/OmaWatch/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Assets.Scripts.OmaWatch.Input;
+using Assets.Scripts.OmaWatch.Util;
+using UnityEngine;
+
+namespace Assets.Scripts.OmaWatch
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float Speed = 3f;
+
+        private DefaultInputActions _defaultInput;
+
+        private void Awake()
+        {
+            _defaultInput = new DefaultInputActions();
+        }
+
+        private void OnEnable()
+        {
+            _defaultInput.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _defaultInput.Player.Disable();
+        }
+
+        private void Update()
+        {
+            var translation = _defaultInput.Player.Move.ReadValue<Vector2>().normalized * Speed * Time.deltaTime;
+            transform.Translate(translation.ToVector3());
+        }
+    }
+}
---
+++ OmaWatchUnity/Assets/Scripts/OmaWatch/Util/Vector2Extensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.OmaWatch.Util
+{
+    public static class Vector2Extensions
+    {
+        public static Vector3 ToVector3(this Vector2 vector2, float z = 0)
+        {
+            return new Vector3(vector2.x, vector2.y, z);
+        }
+    }
+}
