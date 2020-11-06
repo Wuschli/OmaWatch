40d61d6aeb9260e68115c82653062b4dcc77a8be
Arne von Öhsen <arne.von.oehsen@kingart-games.com>
2020-11-07 00:11:47 +0100

    added nav mesh test

---
+++ OmaWatchUnity/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveScript : MonoBehaviour
+{
+    public GameObject moveTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var comp = GetComponent<NavMeshAgent>();
+        comp.updateRotation = false;
+        comp.updateUpAxis = false;
+        //comp.Move(moveTarget.transform.position);
+        comp.SetDestination(moveTarget.transform.position);
+        comp.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
