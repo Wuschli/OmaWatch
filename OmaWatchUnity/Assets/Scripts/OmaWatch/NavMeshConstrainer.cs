bea208685bcc7b852c7ff350af21d3b00b63cf8e
Arne von Öhsen <arne.von.oehsen@kingart-games.com>
2020-11-07 13:06:22 +0100

    contstrain player to navmesh feeling good

---
+++ OmaWatchUnity/Assets/Scripts/OmaWatch/NavMeshConstrainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.OmaWatch
+{
+    public class NavMeshConstrainer : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (NavMesh.SamplePosition(transform.position, out var hit, 1.0f, NavMesh.AllAreas))
+            {
+                transform.position = hit.position;
+            }
+        }
+    }
+}
