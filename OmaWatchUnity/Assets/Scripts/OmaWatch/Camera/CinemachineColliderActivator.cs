604cdda2bb962ac421de851c56c86b8b1d1971b8
Lukas Höppner <lukas.hoeppner@gmail.com>
2020-11-14 13:10:49 +0100

    Zoom out to Construction Site if camera is close to it #1

---
+++ OmaWatchUnity/Assets/Scripts/OmaWatch/Camera/CinemachineColliderActivator.cs
@@ -0,0 +1,37 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace Assets.Scripts.OmaWatch.Camera
+{
+    [RequireComponent(typeof(CinemachineVirtualCamera))]
+    public class CinemachineColliderActivator : MonoBehaviour
+    {
+        public int TargetPriority = 20;
+
+        private CinemachineVirtualCamera _virtualCamera;
+
+        protected void OnEnable()
+        {
+            if (GetComponent<Collider2D>() == null)
+                Debug.LogWarning($"{nameof(CinemachineColliderActivator)} needs an attached Collider!");
+            _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+            _virtualCamera.Priority = 0;
+        }
+
+        protected void OnTriggerEnter2D(Collider2D other)
+        {
+            var player = other.gameObject.GetComponent<AbstractPlayerController>();
+            if (player == null)
+                return;
+            _virtualCamera.Priority = TargetPriority;
+        }
+
+        protected void OnTriggerExit2D(Collider2D other)
+        {
+            var player = other.gameObject.GetComponent<AbstractPlayerController>();
+            if (player == null)
+                return;
+            _virtualCamera.Priority = 0;
+        }
+    }
+}
