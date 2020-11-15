83bff7337dbf19a170d2565fc46974ed836e3b5b
Lukas Höppner <lukas.hoeppner@gmail.com>
2020-11-15 22:38:02 +0100

    Reimplement Loot-At controls for gamepad and mouse #16

---
+++ OmaWatchUnity/Assets/Scripts/Common/Input/OffsetVector2Processor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Assets.Scripts.Common.Input
+{
+#if UNITY_EDITOR
+    [InitializeOnLoad]
+#endif
+    public class OffsetVector2Processor : InputProcessor<Vector2>
+    {
+        [Tooltip("Offset value to shift the incoming Vector2's X component by.")]
+        public float X = 1;
+
+        [Tooltip("Offset value to shift the incoming Vector2's Y component by.")]
+        public float Y = 1;
+
+#if UNITY_EDITOR
+        static OffsetVector2Processor()
+        {
+            Initialize();
+        }
+#endif
+
+        public override Vector2 Process(Vector2 value, InputControl control)
+        {
+            var result = value + new Vector2(X, Y);
+
+            Debug.Log(result);
+            return result;
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        static void Initialize()
+        {
+            InputSystem.RegisterProcessor<OffsetVector2Processor>();
+        }
+    }
+}
