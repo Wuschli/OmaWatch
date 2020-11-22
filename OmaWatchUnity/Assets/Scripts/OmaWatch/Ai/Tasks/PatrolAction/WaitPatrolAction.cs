25eb5a6127ef3b0911d28cfcaff4ce68094b46d6
Arne von Öhsen <arne.von.oehsen@kingart-games.com>
2020-11-22 22:15:57 +0100

    agents can wait at patrol points

---
+++ OmaWatchUnity/Assets/Scripts/OmaWatch/Ai/Tasks/PatrolAction/WaitPatrolAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.OmaWatch.Ai.Tasks.PatrolAction
+{
+    public class WaitPatrolAction : MonoBehaviour, IPatrolAction
+    {
+        public float WaitTime;
+
+        public async Task Execute(AgentBehaviour agent, CancellationToken token)
+        {
+            await Task.Delay((int) (1000 * WaitTime), token);
+        }
+    }
+}
