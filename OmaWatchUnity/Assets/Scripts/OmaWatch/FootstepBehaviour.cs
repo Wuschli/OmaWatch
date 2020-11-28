995c0af904b9c43f142cc876615b0e084285b5ab
Lukas Höppner <lukas.hoeppner@gmail.com>
2020-11-28 23:28:34 +0100

    Create guard animations

---
+++ OmaWatchUnity/Assets/Scripts/OmaWatch/FootstepBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.OmaWatch
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class FootstepBehaviour : MonoBehaviour
+    {
+        public AudioClip[] FootStepClips;
+
+        public void OnFootstep()
+        {
+            var index = Random.Range(0, FootStepClips.Length);
+            GetComponent<AudioSource>().PlayOneShot(FootStepClips[index]);
+        }
+    }
+}
