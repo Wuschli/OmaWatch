3bc82a0bd0c0634b83db7b87dc01f50f79cc952e
Lukas Höppner <lukas.hoeppner@gmail.com>
2020-11-29 15:48:52 +0100

    Allow to change user display name

--- OmaWatchUnity/Assets/Scripts/UI/WinController.cs
+++ OmaWatchUnity/Assets/Scripts/UI/WinController.cs
@@ -5,6 +5,7 @@ using Assets.Scripts.OmaWatch;
 using Assets.Scripts.OmaWatch.GamePlay;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.UI
 {
@@ -13,10 +14,20 @@ namespace Assets.Scripts.UI
         public Transform LeaderboardRoot;
         public LeaderboardEntry LeaderboardEntryPrefab;
         public TMP_Text YourTimeText;
+        public TMP_InputField DisplayNameInputField;
+        public Button DisplayNameSubmitButton;
 
         protected void OnEnable()
         {
             StartUp().FireAndForget();
+        }
+
+        public void SubmitNewDisplayName()
+        {
+            var displayName = DisplayNameInputField.text;
+            if (string.IsNullOrEmpty(displayName))
+                return;
+            SubmitNewDisplayNameAsync(displayName).FireAndForget();
         }
 
         public void BackToMenu()
@@ -26,6 +37,7 @@ namespace Assets.Scripts.UI
 
         private async Task StartUp()
         {
+            DisplayNameSubmitButton.interactable = false;
             foreach (Transform child in LeaderboardRoot)
             {
                 Destroy(child.gameObject);
@@ -34,6 +46,8 @@ namespace Assets.Scripts.UI
             YourTimeText.text = $"Your Time: {LevelCoordinator.Instance.Score.ToScoreDisplay()}";
             if (!PlayFabManager.Instance.IsLoggedIn)
                 await PlayFabManager.Instance.Login();
+
+            DisplayNameInputField.text = await PlayFabManager.Instance.GetLocalAccountDisplayName();
             var leaderboard = await PlayFabManager.Instance.GetLeaderboard("High Score");
 
             foreach (var leaderboardEntry in leaderboard.Leaderboard.OrderBy(e => e.Position))
@@ -41,6 +55,17 @@ namespace Assets.Scripts.UI
                 var instance = Instantiate(LeaderboardEntryPrefab, LeaderboardRoot, false);
                 instance.Entry = leaderboardEntry;
             }
+
+            DisplayNameSubmitButton.interactable = true;
+        }
+
+        private async Task SubmitNewDisplayNameAsync(string displayName)
+        {
+            DisplayNameSubmitButton.interactable = false;
+            if (!PlayFabManager.Instance.IsLoggedIn)
+                await PlayFabManager.Instance.Login();
+            await PlayFabManager.Instance.SetDisplayName(displayName);
+            await StartUp();
         }
     }
 }
